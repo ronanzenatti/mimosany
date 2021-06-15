f74c9ec312743fbdf2c6df1ccd0baaf2fb4abfaa
Ronan Adriel Zenatti <ronan.zenatti@etec.sp.gov.br>
2021-06-15 01:01:52 -0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('cpf')->unique()->nullable();
+            $table->string('cpf')->nullable();
             $table->string('cellphone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
